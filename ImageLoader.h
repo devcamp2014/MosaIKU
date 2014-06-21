2c23519abf44cd79a4c3fe481828439615619254
Kenichi HIROSE <seroron@outlook.com>
2014-06-21 09:09:27 +0900

    trim image

--- ImageLoader.h
+++ ImageLoader.h
@@ -5,10 +5,12 @@
 class ImageLoader {
 public:
   cv::Mat operator()(std::string name, int width, int height) {
+    //    std::cout << name << std::endl;
+
     cv::Mat orig_img = cv::imread(name, 1);
 
     cv::Mat resize_img(width, height, orig_img.type());
     cv::resize(orig_img, resize_img, resize_img.size(), cv::INTER_CUBIC);
-    return resize_img;
+    return resize_img;    
   }
 };
